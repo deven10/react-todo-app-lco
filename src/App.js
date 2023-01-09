dca5e30de05bd5bc3bb22aea65e1b955f5d091b0
Deven Umrania <umraniadeven10@gmail.com>
2023-01-09 19:00:24 +0530

    Update: Basic Todo App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { useReducer } from 'react';
+import { Container } from 'reactstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
-function App() {
+import { TodoContext } from './context/TodoContext';
+import todoReducer from './context/reducer';
+import TodoForm from './Component/TodoForm';
+import Todos from './Component/Todos';
+
+const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, []);
   return (
-    <div className="App">
-      hello
-    </div>
-  );
+    <TodoContext.Provider value={{todos, dispatch}}>
+      <Container fluid>
+        <h1>TODO App with Context API</h1>
+        <TodoForm/>
+        <div className='todos'>
+          <Todos/>
+        </div>
+      </Container>
+    </TodoContext.Provider>
+  )
 }
 
 export default App;
---
+++ src/Component/TodoForm.js
@@ -0,0 +1,59 @@
+import React, { useState, useContext} from 'react';
+import {
+    FormGroup,
+    Form,
+    Input,
+    Button,
+    InputGroupAddOn,
+    InputGroup
+} from 'reactstrap';
+
+import {v4} from "uuid";
+import {ADD_TODO} from "../context/action.types";
+import {TodoContext} from '../context/TodoContext';
+
+const TodoForm = () => {
+    const [todoString, setTodoString] = useState("");
+    const {dispatch} = useContext(TodoContext);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(todoString === ""){
+            return alert("Please enter a todo");
+        }
+
+        const todo = {
+            todoString,
+            id: v4()
+        }
+
+        dispatch({
+            type: ADD_TODO,
+            payload: todo
+        })
+
+        setTodoString("");
+    }
+
+    return (
+        <Form autocomplete="off" onSubmit={handleSubmit} className='form__todo'>
+            <FormGroup>
+                <InputGroup>
+                    <Input
+                        type="text"
+                        name="todo"
+                        id="todo"
+                        placeholder='Your Next Todo'
+                        value={todoString}
+                        onChange={e => setTodoString(e.target.value)}
+                    />
+                    {/* <InputGroupAddOn addonType="prepend"> */}
+                        <Button  color="warning">Add Todo</Button>
+                    {/* </InputGroupAddOn> */}
+                </InputGroup>
+            </FormGroup>
+        </Form>
+    )
+}
+
+export default TodoForm;
